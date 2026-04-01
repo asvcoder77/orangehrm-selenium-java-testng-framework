0a72f09ccbb44a9168bbf7b71629cdeecf573606
Vishnu A S <asvishnukaruvannur7@gmail.com>
2026-04-01 17:00:44 +0530

    Separated basetest into Driverfactory and basetest

--- src/test/java/com/orange/base/BaseTest.java
+++ src/test/java/com/orange/base/BaseTest.java
@@ -1,89 +1,45 @@
 package com.orange.base;
 
-
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 
+import com.orange.factory.DriverFactory;
 import com.orange.listeners.TestListeners;
 import com.orange.utils.Config;
 import com.orange.utils.Constants;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 @Listeners(TestListeners.class)
 public class BaseTest {
 
-	private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
-	
-	@BeforeSuite
-	public void setUpConfig() {
-		// Loads configuration from the properties file and system properties.
-		// This only initializes settings required for the framework.
-		// It does NOT start the automation or browser.
-		Config.initialize();		
-		
-	}
-	@BeforeMethod
-	public void setDriver(ITestContext ctx) throws MalformedURLException, URISyntaxException {
-		if (Boolean.parseBoolean(Config.get(Constants.GRID_ENABLED))) {
-            DriverManager.setDriver(getRemoteDriver());
-        } else {
-            DriverManager.setDriver(getLocalDriver());
-        }
-		WebDriver driver = DriverManager.getDriver();
-		driver.manage().window().maximize();
-		driver.get(Config.get(Constants.ORANGE_HRM_URL));
+    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
 
-        ctx.setAttribute(Constants.DRIVER, DriverManager.getDriver());		
-	}
-	private RemoteWebDriver getRemoteDriver() throws MalformedURLException, URISyntaxException {
-		Capabilities capabilities=new ChromeOptions();
-		if (Constants.FIREFOX.equalsIgnoreCase(Config.get(Constants.BROWSER))) {
-            capabilities = new FirefoxOptions();
-        }
-		String url = String.format(
-                Config.get(Constants.GRID_URL_FORMAT),
-                Config.get(Constants.GRID_HUB_HOST)
-        );
-		log.info("Grid URL: {}", url);
-        return new RemoteWebDriver(new URI(url).toURL(), capabilities);
-	}
-//	private ChromeDriver getLocalDriver() {
-//		WebDriverManager.chromedriver().setup();
-//		return new ChromeDriver();
-//	}
-	private WebDriver getLocalDriver() {
-		String browser = Config.get(Constants.BROWSER);
-		if("firefox".equalsIgnoreCase(browser)) {
-			WebDriverManager.firefoxdriver().setup();
-			return new FirefoxDriver();
-		}
-		else {
-			WebDriverManager.chromedriver().setup();
-			return new ChromeDriver();
-		}
-	}
-	@AfterMethod
-	public void quitDriver() {
-		DriverManager.getDriver().quit();
-		DriverManager.unload();
-	}
+    @BeforeSuite
+    public void setUpConfig() {
+        Config.initialize();
+    }
+
+    @BeforeMethod
+    public void setDriver(ITestContext ctx) {
+
+        // Use DriverFactory instead of local methods
+        DriverManager.setDriver(DriverFactory.createDriver());
+
+        WebDriver driver = DriverManager.getDriver();
+        driver.manage().window().maximize();
+        driver.get(Config.get(Constants.ORANGE_HRM_URL));
+
+        ctx.setAttribute(Constants.DRIVER, driver);
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        DriverManager.getDriver().quit();
+        DriverManager.unload();
+    }
 }
-	
